1557ff7f8ca42dbc17b74f03aaf541746faad3e1
wujing <250102331@qq.com>
2017-04-19 17:50:57 +0800

    Merge branch 'master' of https://github.com/baiqingchun/aifangjin
    # Conflicts:
    #	index.html

--- data.js
+++ data.js
@@ -16,11 +16,74 @@ var aData = {
         },
         {
             img:3,
-            bank:"招商银行",
+            bank:"中信银行",
             rate:5.8,
             mortgageSituation:false,
             loan:90
-        }
+        },
+        {
+            img:4,
+            bank:"东亚银行",
+            rate:5.5,
+            mortgageSituation:false,
+            loan:60
+        },
+        {
+            img:5,
+            bank:"建设银行",
+            rate:5.6,
+            mortgageSituation:true,
+            loan:80
+        },
+        {
+            img:6,
+            bank:"南京银行",
+            rate:5.8,
+            mortgageSituation:false,
+            loan:90
+        }/*,
+        {
+            img:7,
+            bank:"浦发银行",
+            rate:5.5,
+            mortgageSituation:false,
+            loan:60
+        },
+        {
+            img:8,
+                bank:"光大银行",
+            rate:5.6,
+            mortgageSituation:true,
+            loan:80
+        },
+        {
+            img:9,
+            bank:"中信银行",
+            rate:5.8,
+            mortgageSituation:false,
+            loan:90
+        },
+        {
+            img:10,
+            bank:"东亚银行",
+            rate:5.5,
+            mortgageSituation:false,
+            loan:60
+        },
+        {
+            img:11,
+            bank:"建设银行",
+            rate:5.6,
+            mortgageSituation:true,
+            loan:80
+        },
+        {
+            img:12,
+            bank:"南京银行",
+            rate:5.8,
+            mortgageSituation:false,
+            loan:90
+        }*/
     ],
     information:[
         {
@@ -33,6 +96,37 @@ var aData = {
         },
         {
 
+        }
+    ],
+    requirement:[
+        {
+           message: "白日依山尽，黄河入海流。"
+
+        },
+        {
+            message: "白日依山尽，黄河入海流。欲穷千里目，更上一层楼白日依山尽，黄河入海流。欲穷千里目，更上一层楼"
+        },
+        {
+            message: "白日依山尽，黄河入海流。欲穷千里目，更上一层楼"
+        },
+        {
+            message: "白日依山尽，黄河入海流。欲穷千里目，更上一层楼"
+        }
+    ],
+    file:[
+        {
+            message: "床前明月光，疑是地上霜。"
+
+        },
+        {
+            message: "窗前明月光，疑是地上霜，举头望明月，低头思故乡。"
+        },
+        {
+            message: "床前明月光，疑是地上霜。举头望明月"
+
+        },
+        {
+            message: "窗前明月光，疑是地上霜."
         }
     ]
 }
